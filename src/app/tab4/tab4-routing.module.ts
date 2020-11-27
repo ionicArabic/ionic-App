c231545bc9095516ff9ea85bb19494bb203fcc83
ionicArabic <ionicArabic@gmail.com>
2020-11-28 02:31:10 +0300

    updated

--- src/app/tab4/tab4-routing.module.ts
+++ src/app/tab4/tab4-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { Tab1Page } from './tab1.page';
+import { Routes, RouterModule } from '@angular/router';
+
+import { Tab4Page } from './tab4.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: Tab1Page,
-  },
+    component: Tab4Page
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class Tab1PageRoutingModule {}
+export class Tab4PageRoutingModule {}
